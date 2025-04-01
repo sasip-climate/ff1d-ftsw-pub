253737ef4c07b905197f1caf5b5bb5944abc73c6
Nicolas Mokus <nmks@tuta.io>
2025-04-01 22:34:13 +0200

    TST(data): add some basic tests
    Add tests covering:
    * checking there are the right amount of data subdirs
    * checking the existence of a manifest file for all of these subdirs
    * checking all files listed on the manifest exist
    * checking all files listed on the manifest are read and non-empty

---
+++ tests/test_data.py
@@ -0,0 +1,49 @@
+import importlib
+import tomllib
+
+import pytest
+
+import ff1d_ftsw_pub.lib.plot as plot
+
+data_root = "ff1d_ftsw_pub.data"
+nb_figures = 1
+
+
+def get_manifest_handle(subdir: importlib.resources.abc.Traversable):
+    manifest_name = "manifest.toml"
+    return subdir.joinpath(manifest_name)
+
+
+def load_manifest(subdir: importlib.resources.abc.Traversable):
+    with open(get_manifest_handle(subdir), "rb") as file:
+        return tomllib.load(file)
+
+
+def test_total_datasets():
+    data_path = importlib.resources.files(data_root)
+    sub_folders = list(data_path.iterdir())
+    assert len(sub_folders) == nb_figures
+
+
+@pytest.mark.parametrize("subdir", importlib.resources.files(data_root).iterdir())
+def test_all_manifests_exist(subdir: importlib.resources.abc.Traversable):
+    assert get_manifest_handle(subdir).is_file()
+
+
+@pytest.mark.parametrize("subdir", importlib.resources.files(data_root).iterdir())
+def test_all_files_exist(subdir: importlib.resources.abc.Traversable):
+    manifest = load_manifest(subdir)
+    for filetype in manifest.values():
+        for handle in filetype["files"]:
+            assert subdir.joinpath(handle).is_file()
+
+
+@pytest.mark.parametrize("subdir", importlib.resources.files(data_root).iterdir())
+def test_files_are_loaded(subdir: importlib.resources.abc.Traversable):
+    manifest = load_manifest(subdir)
+    data = plot.load_data(subdir)
+    n_files = 0
+    for file_format in manifest:
+        for file in manifest[file_format]["files"]:
+            n_files += 1
+    assert n_files == len(data)
