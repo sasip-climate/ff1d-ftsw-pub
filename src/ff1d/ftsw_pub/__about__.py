69fab0414eb9442564754a791592f42c46474e0a
Nicolas Mokus <nmks@tuta.io>
2025-04-01 15:39:49 +0200

    MAINT: flatten package structure

--- src/ff1d/ftsw_pub/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present Nicolas Mokus <nmks@tuta.io>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- src/ff1d/ftsw_pub/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present Nicolas Mokus <nmks@tuta.io>
-#
-# SPDX-License-Identifier: MIT
