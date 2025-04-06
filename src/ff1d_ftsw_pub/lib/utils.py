d4b3b09ce9babb5d72d02afc9a5958f88234b452
Nicolas Mokus <nmks@tuta.io>
2025-04-06 13:57:39 +0200

    ENH: add figure key to number matcher

---
+++ src/ff1d_ftsw_pub/lib/utils.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class FigureMatcher(enum.Enum):
+    SCHEMATICS = 1
+    FRACTURE_SEARCH = 2
+    STRAIN_FRACTURE = 3
+    SIMPLE_EXAMPLE = 4
+    QUAD_REGIONS = 5
+    QUAD_REGIONS_DISP = 6
+    JOINT_DENSITY = 7
+    ENS_AMPLITUDE = 8
+    ENS_CONSTANTS = 9
