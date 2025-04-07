073b61707e9fec25f5edf053f2d0dfc2f805ae87
Nicolas Mokus <nmks@tuta.io>
2025-04-07 14:21:42 +0200

    ENH: add map between figure numbers and labels

--- src/ff1d_ftsw_pub/lib/utils.py
+++ src/ff1d_ftsw_pub/lib/utils.py
@@ -1,26 +1,4 @@
 import enum
-import json
-
-import numpy
-
-
-def read_json(handle: pathlib.Path) -> dict:
-    with open(handle) as f:
-        values = json.load(f)
-    return values
-
-
-def read_csv(handle: pathlib.Path):
-    return np.loadtxt(handle, delimiter=",")
-
-
-class FileFormat(enum.Enum):
-    csv = read_csv
-    json = read_json
-    npz = np.load
-
-    def __call__(self, handle: pathlib.Path):
-        return self.value(handle)
 
 
 class FigureMatcher(enum.Enum):
